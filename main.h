ce0b66bf5804415b9571dda8029ead50be8802d8
frankmombo <fumbu.com@gmail.com>
2022-04-19 22:24:14 +0300

    header file

--- main.h
+++ main.h
@@ -1,6 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+typedef struct specifier
+{
+	char *valid;
+	int (*f)(va_list);
+} spec;
+int _printf(const char *format, ...);
+int print_c(va_list args);
+int print_s(va_list args);
+int print_d(va_list args);
 int _putchar(char c);
 int _printf(const char *format, ...);
 
